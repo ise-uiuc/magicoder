100e352c5bec37fe0d481fe468c6f0a8c2a4588c
Yuxiang Wei <yuxiang630.wei@gmail.com>
2023-11-10 14:45:33 -0600

    feat: training and humaneval evaluation code

---
+++ experiments/run_evalplus.py
@@ -0,0 +1,104 @@
+import itertools
+from dataclasses import dataclass
+from pathlib import Path
+from typing import cast
+
+from evalplus.data import get_human_eval_plus, write_jsonl
+from tqdm.auto import tqdm
+from transformers import HfArgumentParser
+
+from magicoder.llm_wrapper import GenerationConfig, get_model_context
+from magicoder.utils import chunked
+
+
+@dataclass(frozen=True)
+class Args:
+    model_key: str
+    save_path: str
+
+    n_batches: int
+    n_problems_per_batch: int
+    n_samples_per_problem: int
+    prompted: bool
+
+    model_name_or_path: str | None = None
+
+    # @property
+    # def n_samples(self) -> int:
+    #     return self.n_batches * self.batch_size
+
+
+PROMPT = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+{instruction}
+
+### Response:
+"""
+
+
+def form_wizardcoder_prompt(prompt: str) -> str:
+    # return (
+    #     "Below is an instruction that describes a task. "
+    #     "Write a response that appropriately completes the request.\n\n"
+    #     f"### Instruction:\nCreate a Python script for this problem:\n```python\n{prompt}```\n\n### Response:\n```python\n"
+    # )
+    return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+
+### Instruction:
+Implement the following code:
+```python
+{prompt}
+```
+
+### Response:
+```python
+"""
+
+
+def main():
+    parser = HfArgumentParser((Args, GenerationConfig))
+    args, generation_config = cast(
+        tuple[Args, GenerationConfig],
+        parser.parse_args_into_dataclasses(),
+    )
+    problems = get_human_eval_plus()
+    state = get_model_context(args.model_key, args.model_name_or_path)
+
+    problems_chunked = list(chunked(list(problems.items()), args.n_problems_per_batch))
+    iter = itertools.product(problems_chunked, range(args.n_batches))
+    n_total = len(problems_chunked) * args.n_batches
+
+    Path(args.save_path).write_text("")
+    for task_id_and_problems, batch_idx in tqdm(iter, total=n_total):
+        task_ids, problems = zip(*task_id_and_problems)
+        prompts = [problem["prompt"] for problem in problems]
+        if args.prompted:
+            prompts = [form_wizardcoder_prompt(prompt) for prompt in prompts]
+        print("PROMPT")
+        print(prompts[-1])
+        all_prompts = prompts * args.n_samples_per_problem
+        all_task_ids = task_ids * args.n_samples_per_problem
+        response = state.complete(generation_config, all_prompts)
+        completions = response.decoded_outputs
+        assert len(problems) <= args.n_problems_per_batch
+        assert len(completions) == len(problems) * args.n_samples_per_problem
+        print("COMPLETION")
+        print(completions[-1])
+        samples = [
+            dict(
+                task_id=task_id,
+                completion=completion[
+                    : index
+                    if (index := completion.find("```")) != -1
+                    else len(completion)
+                ],
+            )
+            for task_id, completion in zip(all_task_ids, completions)
+        ]
+        write_jsonl(args.save_path, samples, append=True)
+
+
+if __name__ == "__main__":
+    main()
--- src/magicoder/train.py
+++ src/magicoder/train.py
@@ -16,49 +16,6 @@ from magicoder.llm_wrapper import (  # ChatPiece,; ChatTokenizationContext,
 )
 from magicoder.utils import N_CORES
 
-# from evalplus.data import get_human_eval
-# from trl import DPOTrainer
-
-# from peft import LoraConfig, TaskType, get_peft_model
-
-# DEVICE_MAP = {
-#     "model.embed_tokens": 0,
-#     "model.layers.0": 1,
-#     "model.layers.1": 1,
-#     "model.layers.2": 1,
-#     "model.layers.3": 1,
-#     "model.layers.4": 1,
-#     "model.layers.5": 1,
-#     "model.layers.6": 1,
-#     "model.layers.7": 1,
-#     "model.layers.8": 1,
-#     "model.layers.9": 1,
-#     "model.layers.10": 1,
-#     "model.layers.11": 1,
-#     "model.layers.12": 1,
-#     "model.layers.13": 1,
-#     "model.layers.14": 1,
-#     "model.layers.15": 1,
-#     "model.layers.16": 1,
-#     "model.layers.17": 1,
-#     "model.layers.18": 1,
-#     "model.layers.19": 1,
-#     "model.layers.20": 1,
-#     "model.layers.21": 1,
-#     "model.layers.22": 1,
-#     "model.layers.23": 1,
-#     "model.layers.24": 1,
-#     "model.layers.25": 1,
-#     "model.layers.26": 1,
-#     "model.layers.27": 1,
-#     "model.layers.28": 1,
-#     "model.layers.29": 1,
-#     "model.layers.30": 1,
-#     "model.layers.31": 1,
-#     "model.norm": 1,
-#     "lm_head": 1,
-# }
-
 
 @dataclass(frozen=True)
 class ModelArguments:
@@ -66,17 +23,19 @@ class ModelArguments:
     model_name_or_path: str | None = None
 
 
-@dataclass(frozen=True)
-class DataArguments:
-    data_path: str
+# PROMPT = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
+# ### Instruction:
+# {instruction}
 
-PROMPT = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
+# ### Response:
+# """
 
-### Instruction:
-{instruction}
+MAGICODER_PROMPT = """Write a solution to the following programming problem.
 
-### Response:
+[Problem]
+{problem}
+[Solution]
 """
 
 PROMPT_DICT = {
@@ -97,11 +56,7 @@ PROMPT_DICT = {
 IGNORED_INDEX = -100
 
 
-MAX_TRAINING_SEQ_LENGTH = 1180
-PAD_TO_MAX_LENGTH = False
-
-
-def get_map_raw_dataset(context: TokenizationContext):
+def get_map_raw_dataset(args: "Args", context: TokenizationContext):
     def map_raw_dataset(examples: dict[str, list[str]]) -> dict:
         prompts = examples["prompt"]
         completions = examples["completion"]
@@ -122,17 +77,18 @@ def get_map_raw_dataset(context: TokenizationContext):
             )
         ]
         exceeding_length = [
-            len(input_id) > MAX_TRAINING_SEQ_LENGTH
+            len(input_id) > args.max_training_seq_length
             for input_id in untruncated_input_ids
         ]
         input_ids = [
-            input_id[:MAX_TRAINING_SEQ_LENGTH] for input_id in untruncated_input_ids
+            input_id[: args.max_training_seq_length]
+            for input_id in untruncated_input_ids
         ]
         # NOTE: no need to set EOF to IGNORED_INDEX as it is *implicitly* ignored inside
         # the model.forward that shifts the logits left by 1
         labels = [
             (list(map(lambda _: IGNORED_INDEX, instruction_ids)) + response_ids)[
-                :MAX_TRAINING_SEQ_LENGTH
+                : args.max_training_seq_length
             ]
             for instruction_ids, response_ids in zip(
                 prompt_id_batches, completion_id_batches
@@ -153,14 +109,16 @@ def get_map_raw_dataset(context: TokenizationContext):
     return map_raw_dataset
 
 
-def get_data_collator(pad_token_id: int):
+def get_data_collator(args: "Args", pad_token_id: int):
     """Pad input_ids to the right, create labels by setting the padding tokens to -100, and
     create attention_mask to ignore the padding tokens"""
 
     def collate(examples: list[dict[str, list[int]]]) -> dict[str, torch.Tensor]:
         input_ids_unpadded = [example["input_ids"] for example in examples]
         labels_unpadded = [example["labels"] for example in examples]
-        padding_length = MAX_TRAINING_SEQ_LENGTH if PAD_TO_MAX_LENGTH else None
+        padding_length = (
+            args.max_training_seq_length if args.pad_to_max_length else None
+        )
         input_ids = pad_sequences(
             input_ids_unpadded, pad_token_id, "right", padding_length=padding_length
         )
@@ -171,9 +129,9 @@ def get_data_collator(pad_token_id: int):
         assert input_ids.shape == labels.shape
         assert len(input_ids) == len(examples)
         # Enforced in `map_raw_dataset`
-        assert input_ids.shape[-1] <= MAX_TRAINING_SEQ_LENGTH
-        if PAD_TO_MAX_LENGTH:
-            assert input_ids.shape[-1] == MAX_TRAINING_SEQ_LENGTH
+        assert input_ids.shape[-1] <= args.max_training_seq_length
+        if args.pad_to_max_length:
+            assert input_ids.shape[-1] == args.max_training_seq_length
 
         return {
             "input_ids": input_ids,
@@ -196,246 +154,95 @@ def get_data_collator(pad_token_id: int):
 
 
 @dataclass(frozen=True)
-class DatasetConfig:
-    dataset_name: str
-    dpo_jsonl_path: str | None = field(default=None)
-    dpo_sft: bool = field(default=False)
-    sub_name: str | None = field(default=None)
-    n_samples: int = field(default=2000)
+class Args:
+    datafile_paths: list[str] = field(default_factory=list)
+    max_training_seq_length: int = field(default=1180)
+    pad_to_max_length: bool = field(default=False)
+    eval_dataset_size: float = field(
+        default=0.05, metadata={"help": "0--1 means ratio, >1 means number of examples"}
+    )
 
 
-def split_python_function_body(code: str) -> tuple[str, str] | None:
-    """Split the code into the function signature + docstring and the function body."""
-    docstring_start = code.find('"""')
-    docstring_end = code.find('"""\n', docstring_start + 3)
-    if docstring_start == -1 or docstring_end == -1:
-        return None
-    docstring_end += 4
-    header, body = code[:docstring_end], code[docstring_end:]
-    return header, body
-
-
-# def map_code_search_net(examples: dict[str, list[str]]) -> dict[str, list[str]]:
-#     func_codes = examples["func_code_string"]
-#     func_codes = [
-#         new_code
-#         for code in func_codes
-#         if (new_code := utils.reformat_python(code)) is not None
+# def map_wizardcoder_reproduce(examples: dict[str, list[str]]) -> dict[str, list[str]]:
+#     prompts = [
+#         PROMPT.format(instruction=instruction.rstrip())
+#         for instruction in examples["instruction"]
 #     ]
-#     parts = filter(lambda x: x is not None, map(split_python_function_body, func_codes))
-#     try:
-#         signatures_and_docstrings, bodies = list(zip(*parts))  # type: ignore # fmt: off
-#     except ValueError:
-#         return {"prompt": [], "completion": []}
-#     # # if len(result) != 2:
-#     # #     breakpoint()
-#     # signatures_and_docstrings, bodies = result
-#     assert len(signatures_and_docstrings) == len(bodies)
-#     return {
-#         "prompt": list[str](signatures_and_docstrings),
-#         "completion": list[str](bodies),
-#     }
+#     completions = [output.lstrip() for output in examples["output"]]
+#     return {"prompt": prompts, "completion": completions}
 
 
-def map_wizardcoder_reproduce(examples: dict[str, list[str]]) -> dict[str, list[str]]:
-    prompts = [
-        PROMPT.format(instruction=instruction.rstrip())
-        for instruction in examples["instruction"]
-    ]
-    completions = [output.lstrip() for output in examples["output"]]
-    return {"prompt": prompts, "completion": completions}
+# def map_code_alpaca(examples: dict[str, list[str]]) -> dict[str, list[str]]:
+#     prompts = [
+#         PROMPT_DICT["prompt_no_input"].format(instruction=instruction)
+#         if input.strip() == ""
+#         else PROMPT_DICT["prompt_input"].format(instruction=instruction, input=input)
+#         for instruction, input in zip(examples["instruction"], examples["input"])
+#     ]
+#     completions = [output.lstrip() for output in examples["output"]]
+#     return {"prompt": prompts, "completion": completions}
 
 
-def map_code_alpaca(examples: dict[str, list[str]]) -> dict[str, list[str]]:
-    prompts = [
-        PROMPT_DICT["prompt_no_input"].format(instruction=instruction)
-        if input.strip() == ""
-        else PROMPT_DICT["prompt_input"].format(instruction=instruction, input=input)
-        for instruction, input in zip(examples["instruction"], examples["input"])
-    ]
-    completions = [output.lstrip() for output in examples["output"]]
-    return {"prompt": prompts, "completion": completions}
-
-
-def map_gpt4_teacher(examples: dict[str, list[str]]) -> dict[str, list[str]]:
-    prompts = [
-        PROMPT.format(instruction=instruction)
-        # PROMPT_DICT["prompt_no_input"].format(instruction=instruction)
-        # if input.strip() == ""
-        # else PROMPT_DICT["prompt_input"].format(instruction=instruction, input=input)
-        for instruction, input in zip(examples["instruction"], examples["input"])
-    ]
-    completions = [
-        f"```python\n{output.strip()}\n```" for output in examples["response"]
-    ]
-    return {"prompt": prompts, "completion": completions}
-
-
-def get_gpt4_teacher(config: DatasetConfig) -> Dataset:
-    dataset = load_dataset(
-        "json",
-        num_proc=N_CORES,
-        data_files=["gpt4-code-instruct-python.jsonl"],
-        split="train",
-    )
-    indices = random.sample(range(len(dataset)), k=config.n_samples)
-    dataset = dataset.select(indices)
-    dataset = dataset.map(
-        map_gpt4_teacher,
-        batched=True,
-        num_proc=N_CORES,
-        remove_columns=dataset.column_names,
-        load_from_cache_file=False,
-    )
-    return dataset
-
-
-def get_wizardcoder_reproduce(config: DatasetConfig) -> Dataset:
-    # mlabonne/Evol-Instruct-Python-26k
-    dataset = load_dataset(
-        "theblackcat102/evol-codealpaca-v1", split="train", num_proc=N_CORES
-    )
-    if config.dpo_jsonl_path is not None:
-        pref_dataset = load_dataset(
-            "json", data_files=[config.dpo_jsonl_path], split="train"
-        )
-
-        def map_pref_data(examples: dict[str, list[str]]) -> dict[str, list[str]]:
-            prompts: list[str] = []
-            outputs: list[str] = []
-            for index, prompt in zip(examples["index"], examples["prompt"]):
-                assert dataset[index]["instruction"] == prompt
-                prompts.append(PROMPT.format(instruction=prompt))
-                outputs.append(dataset[index]["output"])
-            if config.dpo_sft:
-                return {
-                    "prompt": prompts,
-                    "completion": examples["rejected"],
-                }
-            else:
-                return {
-                    "prompt": prompts,
-                    "chosen": examples["chosen"],
-                    # "chosen": examples["chosen"],
-                    "rejected": examples["rejected"],
-                }
-
-        pref_dataset = pref_dataset.map(map_pref_data, batched=True, num_proc=N_CORES)
-        return pref_dataset
-    # indices = random.sample(range(len(dataset)), k=config.n_samples)
-    # dataset = dataset.select(indices)
-    dataset = dataset.map(
-        map_wizardcoder_reproduce,
-        batched=True,
-        num_proc=N_CORES,
-        remove_columns=dataset.column_names,
-        load_from_cache_file=False,
-    )
-    return dataset
-
-
-def get_code_alpaca(config: DatasetConfig) -> Dataset:
-    dataset = load_dataset("sahil2801/CodeAlpaca-20k", split="train", num_proc=N_CORES)
-    # indices = random.sample(range(len(dataset)), k=config.n_samples)
-    # dataset = dataset.select(indices)
-    dataset = dataset.map(
-        map_code_alpaca,
-        batched=True,
-        num_proc=N_CORES,
-        remove_columns=dataset.column_names,
-        load_from_cache_file=False,
-    )
-    return dataset
-
-
-def map_instructional_prompt(examples: dict[str, list[str]]) -> dict[str, list[str]]:
-    prompts = [
-        PROMPT.format(instruction=instruction.rstrip())
-        for instruction in examples["prompt"]
-    ]
-    completions = [output.lstrip() for output in examples["completion"]]
-    return {"prompt": prompts, "completion": completions}
-
-
-# def get_code_search_net(config: DatasetConfig) -> Dataset:
+# def get_wizardcoder_reproduce(config: DatasetConfig) -> Dataset:
+#     # mlabonne/Evol-Instruct-Python-26k
 #     dataset = load_dataset(
-#         "code_search_net", config.sub_name, split="train", num_proc=N_CORES
+#         "theblackcat102/evol-codealpaca-v1", split="train", num_proc=N_CORES
 #     )
-#     print("Size of code_search_net:", len(dataset))
-#     dataset = dataset.select_columns(["func_code_string"])
 #     dataset = dataset.map(
-#         # map_code_search_net,
+#         map_wizardcoder_reproduce,
 #         batched=True,
 #         num_proc=N_CORES,
 #         remove_columns=dataset.column_names,
-#         load_from_cache_file=True,
+#         load_from_cache_file=False,
 #     )
-#     indices = random.sample(range(len(dataset)), k=config.n_samples)
-#     dataset = dataset.select(indices)
 #     return dataset
 
 
-# def get_humaneval(config: DatasetConfig) -> Dataset:
-#     data = get_human_eval()
-#     prompts = [value["prompt"] for value in data.values()]
-#     completions = [value["canonical_solution"] for value in data.values()]
-#     dataset = Dataset.from_dict({"prompt": prompts, "completion": completions})
+# def get_code_alpaca(config: DatasetConfig) -> Dataset:
+#     dataset = load_dataset("sahil2801/CodeAlpaca-20k", split="train", num_proc=N_CORES)
+#     # indices = random.sample(range(len(dataset)), k=config.n_samples)
+#     # dataset = dataset.select(indices)
+#     dataset = dataset.map(
+#         map_code_alpaca,
+#         batched=True,
+#         num_proc=N_CORES,
+#         remove_columns=dataset.column_names,
+#         load_from_cache_file=False,
+#     )
 #     return dataset
 
 
-def get_evol_code(config: DatasetConfig) -> Dataset:
-    dataset = load_dataset(
-        "json", data_files=["evol_code_new_responded.jsonl"], split="train"
-    )
+def get_dataset(args: Args) -> Dataset:
+    dataset = load_dataset("json", data_files=args.datafile_paths, split="train")
 
     # dataset = dataset.remove_columns(["seed"])
     # dataset_old = load_dataset(
     #     "json", data_files=["evol_code_responded.jsonl"], split="train"
     # )
     # dataset = concatenate_datasets([dataset, dataset_old])
+    def map_fn(examples: dict[str, list[str]]) -> dict[str, list[str]]:
+        prompts = [
+            MAGICODER_PROMPT.format(problem=problem) for problem in examples["problem"]
+        ]
+        completions = [output.lstrip() for output in examples["solution"]]
+        return {"prompt": prompts, "completion": completions}
 
-    dataset = dataset.rename_columns(
-        {
-            "instruction": "prompt",
-            "response": "completion",
-        }
+    dataset = dataset.map(
+        map_fn,
+        batched=True,
+        num_proc=N_CORES,
+        remove_columns=dataset.column_names,
     )
-    dataset = dataset.map(map_instructional_prompt, batched=True, num_proc=N_CORES)
-    return dataset
-
-
-def get_complex_instructions(config: DatasetConfig) -> Dataset:
-    dataset = load_dataset(
-        "json",
-        data_files=["complex-instructions.jsonl", "complex-instructions-1108.jsonl"],
-        split="train",
-    )
-    dataset = dataset.rename_columns(
-        {
-            "instruction": "prompt",
-            "response": "completion",
-        }
-    )
-    dataset = dataset.map(map_instructional_prompt, batched=True, num_proc=N_CORES)
     return dataset
 
 
 def train():
-    parser = HfArgumentParser((ModelArguments, TrainingArguments, DatasetConfig))
-    model_args, training_args, dataset_config = cast(
-        tuple[ModelArguments, TrainingArguments, DatasetConfig],
+    parser = HfArgumentParser((ModelArguments, TrainingArguments, Args))
+    model_args, training_args, args = cast(
+        tuple[ModelArguments, TrainingArguments, Args],
         parser.parse_args_into_dataclasses(),
     )
-    if dataset_config.dataset_name == "sahil2801/CodeAlpaca-20k":
-        dataset = get_code_alpaca(dataset_config)
-    elif dataset_config.dataset_name == "gpt4-code-instruct-python.jsonl":
-        dataset = get_gpt4_teacher(dataset_config)
-    elif dataset_config.dataset_name == "evol-instruct":
-        dataset = get_wizardcoder_reproduce(dataset_config)
-    elif dataset_config.dataset_name == "complex-instructions":
-        dataset = get_complex_instructions(dataset_config)
-    else:
-        dataset = get_evol_code(dataset_config)
+    dataset = get_dataset(args)
 
     model_key = model_args.model_key
     if (model_name_or_path := model_args.model_name_or_path) is None:
@@ -444,29 +251,31 @@ def train():
     tokenization_context = TokenizationContext.from_model_key(
         model_key, model_name_or_path
     )
-    if dataset_config.dpo_jsonl_path is None or dataset_config.dpo_sft:
-        map_raw_dataset = get_map_raw_dataset(tokenization_context)
-        train_dataset = dataset.map(
-            map_raw_dataset,
-            batched=True,
-            num_proc=N_CORES,
-            remove_columns=dataset.column_names,
-            load_from_cache_file=False,  # not args.overwrite_cache
-            desc="Running tokenizer on train dataset",
-        )
-        msg = f"#Examples truncated: {sum(train_dataset['exceeding_length'])} / {len(dataset)}"
-        print(msg)
-    else:
-        train_dataset = dataset
+    # if dataset_config.dpo_jsonl_path is None or dataset_config.dpo_sft:
+    map_raw_dataset = get_map_raw_dataset(args, tokenization_context)
+    train_dataset = dataset.map(
+        map_raw_dataset,
+        batched=True,
+        num_proc=N_CORES,
+        remove_columns=dataset.column_names,
+        load_from_cache_file=False,  # not args.overwrite_cache
+        desc="Running tokenizer on train dataset",
+    )
+    msg = f"#Examples truncated: {sum(train_dataset['exceeding_length'])} / {len(dataset)}"
+    print(msg)
+    # else:
+    #     train_dataset = dataset
 
     # Shuffling
     if training_args.eval_steps is None and training_args.evaluation_strategy == "no":
-        train_dataset = train_dataset.shuffle(seed=0)
+        train_dataset = train_dataset.shuffle(seed=training_args.seed)
         eval_dataset = None
     else:
         print("Splitting dataset")
         split_dataset = train_dataset.train_test_split(
-            test_size=0.1, shuffle=True, seed=0
+            test_size=args.eval_dataset_size,
+            shuffle=True,
+            seed=training_args.seed,
         )
         train_dataset = split_dataset["train"]
         eval_dataset = split_dataset["test"]
@@ -483,7 +292,7 @@ def train():
     #     state.model = get_peft_model(state.model, LORA_CONFIG)
 
     print("Parallel mode:", training_args.parallel_mode)
-    data_collator = get_data_collator(state.tokenization_context.pad_token_id)
+    data_collator = get_data_collator(args, state.tokenization_context.pad_token_id)
 
     # if dataset_config.dpo_jsonl_path is not None and not dataset_config.dpo_sft:
     #     print("Ready to do DPO")
