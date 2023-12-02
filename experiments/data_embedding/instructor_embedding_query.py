b7d81ab1e080418de201c68e13798da9a2dc6d79
Zhe Wang <42107973+zhewang2001@users.noreply.github.com>
2023-12-03 01:03:10 +0800

    Update instructor_embedding_query.py

--- experiments/data_embedding/instructor_embedding_query.py
+++ experiments/data_embedding/instructor_embedding_query.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from typing import Literal, cast
-
+from matplotlib.colors import rgb_to_hsv, hsv_to_rgb  
 import matplotlib.pyplot as plt
 import numpy as np
 from appdirs import user_cache_dir
@@ -11,12 +11,18 @@ from sentence_transformers.util import cos_sim
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 from transformers import HfArgumentParser
+from matplotlib.font_manager import FontProperties
 import random
 import sys
 sys.path.append("src/magicoder")  
 from prompt_template import SRC_INSTRUCT_ILLUSTRATION_PROMPT
 from collections import Counter
+from adjustText import adjust_text
+import matplotlib.colors as mcolors
+from matplotlib.colors import to_rgba, to_hex
 
+plt.rcParams['text.usetex'] = True
+plt.rcParams['font.family'] = 'sans-serif'
 
 MEM = Memory(location=user_cache_dir("magicoder-experiments"))
 
@@ -35,6 +41,7 @@ class Args:
     query_instruction: str | None = field(default=None)
     batch_size: int = field(default=32)
     n_clusters: int | None = field(default=None)
+
 
 
 @MEM.cache(ignore=["model", "batch_size"])
@@ -155,43 +162,67 @@ def main():
         assert labels.max() == len(queries) - 1
         assert labels.min() == 0
         n_clusters = len(queries)
-        query_labels = [query[1].split()[0] for query in queries]
+        # query_labels = [query[1].split()[0] for query in queries]
+        query_labels = [query[1] for query in queries]
         label_counts = Counter(labels)
         categories = [query_labels[label] for label in label_counts.keys()]
-        counts = list(label_counts.values())
-        
-        colors = plt.cm.Set3(np.random.permutation(np.linspace(0, 1, len(categories))))
-        color_names = [
-            "Salmon", "Tomato", "Gold", "MediumSeaGreen", "LightSeaGreen",
-            "YellowGreen", "MediumTurquoise", "SlateBlue", "MediumOrchid", "BlueViolet"
+        counts = list(label_counts.values())     
+        colors = plt.cm.tab10(np.random.permutation(np.linspace(0, 1, len(categories))))
+        colors = [
+            "Tomato", "Coral", "Orange", "Gold", "YellowGreen", "MediumSeaGreen", "MediumTurquoise","LightSeaGreen",
+             "RoyalBlue", "MediumOrchid", 
         ]
-        selected_colors = np.random.choice(color_names, len(categories), replace=False)
-        explode = [0.05] * len(counts)
-        fig, ax = plt.subplots(figsize=(18, 10))
+        # selected_colors = np.random.choice(color_names, len(categories), replace=False)
+        selected_colors = np.array(colors[:len(categories)])
+        def adjust_saturation_and_brightness(color, saturation_factor, brightness_factor):
+            """
+            Adjust the saturation and brightness of a color.
+            color: Hex or RGB color
+            saturation_factor: float (less than 1 to desaturate, more than 1 to increase saturation)
+            brightness_factor: float (less than 1 to darken, more than 1 to lighten)
+            """
+            rgba = to_rgba(color)
+            hsv = mcolors.rgb_to_hsv(rgba[:3])
+            hsv[1] *= saturation_factor
+            hsv[2] *= brightness_factor
+            new_rgb = mcolors.hsv_to_rgb(hsv)
+            return to_hex(new_rgb)
+
+        explode = [0.02] * len(counts)
+        fig, ax = plt.subplots(figsize=(13.5, 3))
         
         def calc_angle(percent):
             return 140 - (percent * 3.6) / 2
-        angles = np.cumsum([0] + [calc_angle(count / sum(counts) * 100) for count in counts])
-
-        wedges, texts = ax.pie(counts, colors=selected_colors, explode=explode,startangle=140, wedgeprops={'width': 1, 'edgecolor': 'w'})
+        saturation_factor = 0.55 
+        brightness_factor = 0.85  
+        modified_colors = [adjust_saturation_and_brightness(color, saturation_factor, brightness_factor) for color in selected_colors]
+       
+        wedges, texts = ax.pie(counts, colors=modified_colors, explode=explode,startangle=140, wedgeprops={'width': 0.65, 'edgecolor': 'w'})
         for i, p in enumerate(wedges):
-            ang = (p.theta2 - p.theta1) / 2 + p.theta1
-            y = np.sin(np.deg2rad(ang))
-            x = np.cos(np.deg2rad(ang))
-            horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-            connectionstyle = "angle,angleA=0,angleB={}".format(ang)
-            
-            label = categories[i]  
-            value = "{:.1f}%".format(counts[i] / sum(counts) * 100)  
-            label_and_value = r"$\bf{" + label + "}$" + f" ({value})" 
-            ax.annotate(label_and_value, xy=(x, y), xytext=(1.1*np.sign(x), 1.4*y),
-                        horizontalalignment=horizontalalignment, fontsize=24,
-                        arrowprops=dict(arrowstyle="-", connectionstyle=connectionstyle))
+            angle = (p.theta2 + p.theta1) / 2
+            x = np.cos(np.deg2rad(angle))
+            y = np.sin(np.deg2rad(angle))
+            horizontalalignment = {1: "left", -1: "right"}[int(np.sign(x))]
+            label_words = categories[i].split()
+            label_words = [word for word in label_words if "Problems" not in word]
+            label_words = [word.replace("and", "\&") for word in label_words]
+            font_bold = FontProperties(weight='bold')
 
-        plt.title("Topic Distribution of Our Datasets", fontsize=26, y=-0.2) 
-        plt.subplots_adjust(bottom=0.2) 
+            label = " ".join(label_words)
+            value = "{:.1f}\%".format(counts[i] / sum(counts) * 100) 
+
+            if x>0:
+                label_and_value = f" ({value}) "+label 
+            else:
+                label_and_value = label + f" ({value})"
+            angle_B=-120*np.sign(x)*np.sign(y)
+            xytext = (1.1 * np.sign(x), 1.3*y)
+            ax.annotate(label_and_value, xy=(x, y), xytext=xytext, 
+                horizontalalignment=horizontalalignment, fontsize=18,
+                arrowprops=dict(arrowstyle="-", connectionstyle=f"angle,angleA=0,angleB={angle_B},rad=0"),
+                )
         plt.axis('equal')
-        plt.savefig("/home/zhe/zhe/data_embedding/result/ring_pie_chart.png")
+        plt.savefig("Pie_Chart.png")
       
     
 
